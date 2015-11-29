423cb89e0e290010e3fc1929bfa069c841f899f6
alewi1 <alewi1@umbc.edu>
2015-11-29 14:40:06 -0500

    Update automatic.js

--- play/automatic/automatic.js
+++ play/automatic/automatic.js
@@ -273,7 +273,7 @@ window.reset = function(){
 				else if ((Math.random()<window.RATIO_CIRCLES)) {
 					draggable.color = "circle";
 				}
-				else ((Math.random()<window.RATIO_PENTAGONS)) {
+				else{
 					draggable.color = "pentagon";
 				}
 				draggables.push(draggable);
