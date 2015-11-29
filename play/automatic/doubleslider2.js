f7bceaa2b136abf7c2a03cb00660b31cd6825cd7
alewi1 <alewi1@umbc.edu>
2015-11-29 16:36:57 -0500

    Update doubleslider2.js

--- play/automatic/doubleslider2.js
+++ play/automatic/doubleslider2.js
@@ -26,16 +26,16 @@ function DoubleSlider2(dom,config){
 
 	// Create DOM
 	self.dom.className = "ds";
-	for(var i=0;i<3;i++){
+	for(var i=0;i<4;i++){
 
 		var dom = document.createElement("div");
 		dom.className = "ds_bg";
 		self.dom.appendChild(dom);
-		self.backgrounds[2-i] = dom;
+		self.backgrounds[3-i] = dom;
 
 		// CSS
-		dom.style.backgroundColor = config.backgrounds[2-i].color;
-		dom.style.backgroundImage = "url("+config.backgrounds[2-i].icon+")";
+		dom.style.backgroundColor = config.backgrounds[3-i].color;
+		dom.style.backgroundImage = "url("+config.backgrounds[3-i].icon+")";
 		if(i==0) dom.style.width = "100%";
 
 	}
@@ -65,13 +65,27 @@ function DoubleSlider2(dom,config){
 
 	    	var index = self.draggingSliderIndex;
 	    	var sliderWidth = 0;//0.025;
-	    	if(index==0){
-	    		var edge = self.values[1]-sliderWidth;
-	    		if(val>edge) val=edge;
-	    	}else if(index==1){
-	    		var edge = self.values[0]+sliderWidth;
-	    		if(val<edge) val=edge;
-	    	}
+	    if(index==0){
+	    	var edge = self.values[1]-sliderWidth;
+	    	if(val>edge) val=edge;
+	    }
+	    else if(index==1){
+	    	var back_edge = self.values[0]+ sliderWidth;
+	    	if(val<back_edge) val=back_edge;
+		var front_edge = self.values[2] - sliderWidth;
+			if(val > front_edge) val = front_edge;
+	    }
+   	    else if(index==2){
+	    	var back_edge = self.values[1]+ sliderWidth;
+	    		if(val<back_edge) val=back_edge;
+			var front_edge = self.values[3] - sliderWidth;
+			if(val > front_edge) val = front_edge;
+	    }
+	    else if(index==3){
+	    		var front_edge = self.values[2]+ sliderWidth;
+	    		if(val<front_edge) val=back_edge;
+			
+	     }
 	    	var edge = sliderWidth/2;
     		if(val<edge) val=edge;
 	    	var edge = 1-sliderWidth/2;
@@ -112,14 +126,14 @@ function DoubleSlider2(dom,config){
 	// UI Update
 	self.updateUI = function(){
 
-		for(var i=0;i<2;i++){
+		for(var i=0;i<4;i++){
 			var slider = self.sliders[i];
 			var val = self.values[i];
 			slider.style.left = (400*val - 5)+"px";
 		}
 
 		var bg;
-		var v0=self.values[0]*400, v1=self.values[1]*400;
+		var v0=self.values[0]*400, v1=self.values[1]*400,  v2=self.values[2]*400,  v3=self.values[3]*400, v4=self.values[4]*400;
 		bg = self.backgrounds[0];
 		bg.style.width = v0+"px";
 		bg = self.backgrounds[1];
@@ -127,7 +141,10 @@ function DoubleSlider2(dom,config){
 		bg.style.width = (v1-v0)+"px";
 		bg = self.backgrounds[2];
 		bg.style.left = v1+"px";
-		bg.style.width = (400-v1)+"px";
+		bg.style.width = (v2-v1)+"px";
+		bg = self.backgrounds[3];
+		bg.style.left = v2+"px";
+		bg.style.width = (v3-v2)+"px";
 			 
 
 	};
