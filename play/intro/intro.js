261f0f61e3b6d1823201743fc88722d1b400ecf5
alewi1 <alewi1@umbc.edu>
2015-11-30 00:05:15 -0500

    Update intro.js

--- play/intro/intro.js
+++ play/intro/intro.js
@@ -113,13 +113,13 @@ for(var i=0;i<1280;i+=50){
 var s = new Swinger();
 s.x = 640 + 20;
 s.swing = s.x*0.1;
-s.y = 210 + 40;
+s.y = 210 + 30;
 swingers.push(s);
 
 var s = new Swinger();
 s.x = 640 + 50;
 s.swing = x*0.1;
-s.y = 210 - 40;
+s.y = 210 - 30;
 swingers.push(s);
 
 var s = new Swinger();
